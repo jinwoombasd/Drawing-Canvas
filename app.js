19041c4ef4278bc9a2e246e717206daa0a356f3d
mbasd <115119366+jinwoombasd@users.noreply.github.com>
2024-03-08 17:02:48 +0000

    meme maker

--- app.js
+++ app.js
@@ -2,32 +2,32 @@ const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800; 
+ctx.lineWidth = 2;
 
-// ctx.fillRect(200, 200, 50, 200);
-// ctx.fillRect(400, 200, 50, 200);
-// ctx.lineWidth = 2;
-// ctx.fillRect(300, 300, 50, 100);
-// ctx.fillRect(200, 200, 200, 20);
-// ctx.moveTo(200, 200);
-// ctx.lineTo(325, 100);
-// ctx.lineTo(450, 200);
-// ctx.fill();
 
-// body and arms
-ctx.fillRect(170, 160, 15, 100);
-ctx.fillRect(310, 160, 15, 100);
-ctx.fillRect(220, 160, 60, 200);
- 
-// head
-ctx.arc(250, 100, 50, 0, 2 * Math.PI);
-ctx.fill(); 
+const colors = [
 
-// eyes
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(270, 80, 8, Math.PI, 2 * Math.PI);
-ctx.arc(230, 80, 8, Math.PI, 2 * Math.PI);
-ctx.fill();
+    "#FAEBD7",
+    "#00FFFF",
+    "#F0F8FF",
+    "#7FFFD4",
+    "#F5F5DC",
+    "#8A2BE2",
+    "#7FFF00"
+];
+
+
+function onclick(event){
+    ctx.beginPath();
+    ctx.moveTo(0, 0);
+    const color = colors[Math.floor(Math.random() * colors.length)]
+    ctx.strokeStyle = color;
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke(); 
+}
+
+canvas.addEventListener("click", onclick);
+canvas.addEventListener("mousemove", onclick);
 
 
 
