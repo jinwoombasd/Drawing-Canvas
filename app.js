749ec0ddeccf43443c2ba363145fd9020ba935da
mbasd <115119366+jinwoombasd@users.noreply.github.com>
2024-03-25 16:22:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,31 +3,26 @@ const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 800; 
 ctx.lineWidth = 2;
+let isPainting = false;
 
-
-const colors = [
-
-    "#FAEBD7",
-    "#00FFFF",
-    "#F0F8FF",
-    "#7FFFD4",
-    "#F5F5DC",
-    "#8A2BE2",
-    "#7FFF00"
-];
-
-
-function onclick(event){
-    ctx.beginPath();
-    ctx.moveTo(0, 0);
-    const color = colors[Math.floor(Math.random() * colors.length)]
-    ctx.strokeStyle = color;
-    ctx.lineTo(event.offsetX, event.offsetY);
-    ctx.stroke(); 
+function onMove(event){
+    if (isPainting){
+        ctx.lineTo(event.offsetX, event.offsetY);
+        ctx.stroke();
+        return;
+    }
+    ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-canvas.addEventListener("click", onclick);
-canvas.addEventListener("mousemove", onclick);
+function startDrawing(){
+    isPainting = true;
+}
 
+function cancelDrawing(){
+    isPainting = false;
+}
 
-
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startDrawing);
+canvas.addEventListener("mouseup", cancelDrawing);
+canvas.addEventListener("mouseleave", cancelDrawing);
