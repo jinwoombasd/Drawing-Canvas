e9a8de7e5d1e39a43d64b3522a23073a44a0e2f0
mbasd <115119366+jinwoombasd@users.noreply.github.com>
2024-03-25 16:32:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
-canvas.height = 800; 
-ctx.lineWidth = 2;
+canvas.height = 800;
+ctx.lineWidth = lineWidth.value;
 let isPainting = false;
 
-function onMove(event){
-    if (isPainting){
+function onMove(event) {
+    if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
         ctx.stroke();
         return;
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-function startDrawing(){
+function startDrawing() {
     isPainting = true;
 }
 
-function cancelDrawing(){
+function cancelDrawing() {
     isPainting = false;
+}
+
+function onLineWidthChange(event) {
+    ctx.lineWidth = event.target.value;
 }
 
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startDrawing);
 canvas.addEventListener("mouseup", cancelDrawing);
 canvas.addEventListener("mouseleave", cancelDrawing);
+
+
+lineWidth.addEventListener("change", onLineWidthChange);
